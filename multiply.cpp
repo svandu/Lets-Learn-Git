74133bfb05b8855402c46967d78895b0d3f950d4
svandu <svandu08@gmail.com>
2022-06-24 00:21:20 +0530

    Add new multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a = 15,b=67;
+    int sum = a*b;
+    cout<<"Multiplication of the two number"<<sum;
+    cout<<"We can change the input";
+}
